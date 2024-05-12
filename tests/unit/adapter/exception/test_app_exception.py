8d1cd0cad72788a10b5d2ca6dbd08bca0dc18546
AlexTraveylan <monsieurdemares@gmail.com>
2024-05-12 14:59:10 +0200

    fix test

--- tests/unit/adapter/exception/test_app_exception.py
+++ tests/unit/adapter/exception/test_app_exception.py
@@ -1,17 +1,17 @@
 """
-Test the AppException class
+Test the AlchidanticException class
 
 :author: Alex Traveylan
 :date: 2024
 """
 
-from alchidantic.adapter.exception.app_exception import AppException
+from alchidantic.adapter.exception.app_exception import AlchidanticException
 
 
 def test_app_exception():
     """Test the AppException class."""
 
     message = "Test exception message"
-    exception = AppException(message)
+    exception = AlchidanticException(message)
 
     assert str(exception) == message
